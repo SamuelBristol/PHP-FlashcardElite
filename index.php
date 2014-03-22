f62a3d71fd4cc3dfb7a57daa411037d7a511923e
Samuel Bristol <samuel.d.bristol@gmail.com>
2014-03-21 21:36:17 -0500

    Adds delete field for table entries and modifies table headers to compensate

--- index.php
+++ index.php
@@ -6,11 +6,12 @@
 	if($result){
 ?>
 <table>
-	<tr><th>Title</th><th colspan="2">Description</th></tr>
+	<tr><th>Title</th><th colspan="3">Description</th></tr>
 <?php	while($row = $result->fetch_object()){ ?>
 	<tr><td><?= $row->flashcard_name ?></td>
 		<td><?= $row->flashcard_desc ?></td>
 		<td><a href="edit_card.php?id=<?= $row->flashcard_id ?>">Edit</a></td>
+		<td><a href="delete_card.php?id=<?= $row->flashcard_id ?>">Delete</a></td>
 	</tr>
 <?php }
 			$result->free();
