6768d34d0f5e2720b1032ab41e7f53c286733bd4
Samuel Bristol <samuel.d.bristol@gmail.com>
2014-03-22 03:57:24 -0500

    adds a msg variable and fixes the prepared statement

--- insertCard.php
+++ insertCard.php
@@ -1,15 +1,19 @@
 <?php require 'connect.php';
-if($_POST){
-	$fc_name = $_POST["fc_name"];
-	$fc_desc = $_POST["fc_desc"];
-}
+
+$fc_name = $_POST["fc_name"];
+$fc_desc = $_POST["fc_desc"];
 
 $sql = "INSERT INTO fc_flashcard (flashcard_name, flashcard_desc) VALUES (?, ?)";
 $p_stmt = $dbc->prepare($sql);
 $p_stmt->bind_param("ss", $fc_name, $fc_desc);
-$p_stmt->execute();
+$status = $p_stmt->execute();
+if($status === false){
+	$msg = "Card did not insert successfully.";
+} else {
+	$msg = $fc_name." was inserted successfully.";
+}
 $p_stmt->close();
 $dbc->close();
-echo "<br><br>Your query has run successfully!"
-//redirect to newflashcard.php with a success message or error message
+header("Location: index.php?msg=".$msg);
+die();
 ?>
