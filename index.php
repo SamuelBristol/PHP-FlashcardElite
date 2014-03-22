8bcc40d7ad5eca17379e2117617afc626590924b
Samuel Bristol <samuel.d.bristol@gmail.com>
2014-03-22 03:39:21 -0500

    formats html and adds passed GET msg variable

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
 <?php require("template/header.php"); ?>
 <h2>Welcome, User. Here are your flashcards.</h2>
 <?php require 'connect.php';
-	//Display fc_flashcard table
 	$result = $dbc->query("SELECT * FROM fc_flashcard");
+	if(isset($_GET['msg'])){
+		$msg = $_GET['msg'];
+	} else {
+		$msg = "";
+	}
 	if($result){
+		echo $msg;
 ?>
 <table>
-	<tr><th>Title</th><th colspan="3">Description</th></tr>
+	<tr>
+		<th>Title</th>
+		<th colspan="3">Description</th>
+	</tr>
 <?php	while($row = $result->fetch_object()){ ?>
-	<tr><td><?= $row->flashcard_name ?></td>
+	<tr>
+		<td><?= $row->flashcard_name ?></td>
 		<td><?= $row->flashcard_desc ?></td>
 		<td><a href="edit_card.php?id=<?= $row->flashcard_id ?>">Edit</a></td>
 		<td><a href="delete_card.php?id=<?= $row->flashcard_id ?>">Delete</a></td>
@@ -17,7 +26,7 @@
 			$result->free();
 	} else {
 ?>
-<h3>You don't have any flashcards! <a href="new_card.php"></h3>
+		<h3>You don't have any flashcards! <a href="new_card.php">Create a new one</a></h3>
 <?php
 	}	
 		$dbc->close();
